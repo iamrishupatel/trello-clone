673a7e62484600383d8bc87d639292a3d9cdbf2a
iamrishupatel <rishupatel04@gmail.com>
2023-05-21 23:39:53 +0530

    fix: add owner details in board transformer

--- src/constants/app.constans.ts
+++ src/constants/app.constans.ts
@@ -1,1 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export const DEFAULT_BOARDS_PAGINATION_LIMIT = 100;
+export const ANON_USER_DATA = {
+	isAnonymous: true,
+	displayPicture: '',
+	email: 'anonymous@rlabs.dev',
+	id: uuidv4(),
+	name: 'Anonymous',
+};
