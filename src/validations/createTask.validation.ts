448a1f6e2e370581d2c514ba3053909da4373440
iamrishupatel <rishupatel04@gmail.com>
2023-06-03 21:12:06 +0530

    feat: add priority selector

--- src/validations/createTask.validation.ts
+++ src/validations/createTask.validation.ts
@@ -8,6 +8,7 @@ const createTaskFormSchema = Yup.object().shape({
 	description: Yup.string().max(9000, 'Description cannot be more than 9000 characters'),
 	statusId: Yup.string().required('Status is required'),
 	labels: Yup.array(),
+	priorityId: Yup.string(),
 });
 
 export default createTaskFormSchema;
