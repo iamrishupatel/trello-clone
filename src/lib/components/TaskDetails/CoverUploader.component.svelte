aa9e5b31d5d41b905541cfdeb228e6561f3b970f
iamrishupatel <rishupatel04@gmail.com>
2023-06-08 10:41:11 +0530

    feat: add cover

--- src/lib/components/TaskDetails/CoverUploader.component.svelte
+++ src/lib/components/TaskDetails/CoverUploader.component.svelte
@@ -2,22 +2,16 @@
 	import Icon from '@iconify/svelte';
 	import { Button } from 'flowbite-svelte';
 
+	export let handleFileChange: (e: Event) => void;
+
 	let inpEl: HTMLInputElement;
 	const openUpload = (): void => {
 		inpEl.click();
 	};
-
-	const handleFileChange = (e: any): void => {
-		const file = e.target?.files[0];
-
-		if (file) {
-			console.log(file);
-		}
-	};
 </script>
 
-<Button color="light" class="w-full" on:click={openUpload}>
-	<Icon icon="material-symbols:image" />
+<Button color="light" class="w-full !justify-start text-slate-500" on:click={openUpload}>
+	<Icon icon="material-symbols:image" class="-ml-2" />
 	<span class="ml-2">Cover</span>
 </Button>
 
