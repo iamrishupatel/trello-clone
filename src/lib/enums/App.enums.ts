25c40c03ae0c0eb28a25396870ded7dbe4addd4d
iamrishupatel <rishupatel04@gmail.com>
2023-06-10 11:01:17 +0530

    feat: Filter boards UI

---
+++ src/lib/enums/App.enums.ts
@@ -0,0 +1,8 @@
+// contains enums related to app (frontend)
+export enum BoardFilterOption {
+	PRIVATE = 'Private Boards',
+	PUBLIC = 'Public Boards',
+	MY_BOARDS = 'My Boards',
+	ALL_BOARDS = 'All Boards',
+	COLLABORATED = 'Collaborated Boards',
+}
